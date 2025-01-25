f740f85528b3e8c4e336a8b97cd66cd7a16d2a15
Laura <laura.barona@uao.edu.co>
2025-01-24 20:45:29 -0500

    updated files

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -38,10 +38,28 @@ const updateProduct = async (req, res) => {
             if (!updatedProduct) {
                 return res.status(404).json({ message: "Producto no encontrado" });
             }
-            
+            res.status(200).json({ message: "Producto actualizado correctamente", updatedProduct });
     } catch (error) {
-        
+        res.status(500).json({ message: "Error al actualizar producto", error: error.message });
     }
-}
+};
 
-module.exports = { getProducts, createProduct };
+// delete product (only admin)
+const deleteProduct = async (req, res) => {
+    if (req.user.role !== "admin") {
+        return res.status(403).json({ message: "Acceso denegado, solo el administrador puede eliminar productos"})
+    }
+
+    try {
+        const { id } = req.params;
+        const deletedProduct = await Product.findByIdAndDelete(id);
+        if(!deletedProduct) {
+            return res.status(404).json({ message: "Producto no encontrado" });
+        }
+        res.status(200).json({ message: "Producto eliminado correctamente" });
+    } catch (error) {
+        res.status(500).json({ message: "Error al eliminar producto", error: error.message });
+    }
+};
+
+module.exports = { getProducts, createProduct, updateProduct, deleteProduct };
