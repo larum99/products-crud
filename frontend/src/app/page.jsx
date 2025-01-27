eb9120bb5f1e38251c518275175bd002381852a2
Laura <laura.barona@uao.edu.co>
2025-01-26 21:23:57 -0500

    updated files

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,15 +1,25 @@
 "use client";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
 
+  // Función para manejar la redirección
+  const handleLoginClick = () => {
+    router.push("/login"); // Redirige a la página de login
+  };
+
   return (
     <div className="container text-center">
-      <h1 className="mt-5 text-primary">¡Bootstrap está funcionando!</h1>
-      <button className="btn btn-success">Haz clic aquí</button>
+      <h1 className="mt-5 text-primary">¡Bienvenido a tu App de Gestión de Productos!</h1>
+      <button className="btn btn-success" onClick={handleLoginClick}>
+        Inicia Sesión
+      </button>
     </div>
   );
 }
