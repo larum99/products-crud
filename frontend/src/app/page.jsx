f8c1d3448c20a09105e7cf4368915a17a83a9b6b
Laura <laura.barona@uao.edu.co>
2025-01-29 18:18:16 -0500

    modified files

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -16,7 +16,7 @@ export default function Home() {
 
   return (
     <div className="container-fluid home-container d-flex flex-column justify-content-center align-items-center text-center">
-      <h1 className="home-title text-primary">¡Bienvenido a tu App de Gestión de Productos!</h1>
+      <h1 className="home-title text-dark">¡Bienvenido a tu App de Gestión de Productos!</h1>
       <button className="btn btn-primary mt-3 home-button" onClick={handleLoginClick}>
         Coméncemos
       </button>
